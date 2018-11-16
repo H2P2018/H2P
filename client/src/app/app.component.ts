ad85b80556acb389a63288dfba7162ad3a1c499a
Enrique Pernia <Epernia96@gmail.com>
2018-11-16 20:36:48 +0100

    Setup

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'client';
+  constructor(/*public service: SessionService*/) {}
+
+  ngOnInit() {
+    //this.navbar();
+  }
+
+  submitForm(matchWinner) {}
+
 }
 
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,15 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
+import { ClarityModule } from '@clr/angular';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { RouterModule } from '../../node_modules/@angular/router';
+import { routes } from './routes';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
 
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ClarityModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent]
